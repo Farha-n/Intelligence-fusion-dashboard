533663cb5ec048aa4bed6417b610c9599d78c6fd
Farhan-n <farhanfarooq786000@gmail.com>
2026-04-19 01:05:37 +0530

    Reorganize project structure: move frontend to public/ folder, sample data to data/ folder, clean up legacy files

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(uploadsDir));
-app.use(express.static(path.join(__dirname, "..")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 const storage = multer.diskStorage({
   destination: (_req, _file, cb) => cb(null, uploadsDir),
@@ -168,7 +168,7 @@ app.get("/api/export", async (_req, res) => {
 });
 
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "..", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 app.listen(port, () => {
